14f1f1380fe98d535a7085242b309fcf309171b6
Jurgis Bernatonis <jurgis.bernatonis@protonmail.com>
2022-11-03 17:48:41 +0200

    “#1”

---
+++ README.md
@@ -0,0 +1,3 @@
+# react WEBSITE 
+
+link test
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <About/>
+   
+      
+    </div>
+  );
+}
+
+export default App;
